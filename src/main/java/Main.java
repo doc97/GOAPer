68ce25aa2656172905842017f2af8d6eb81fce52
doc97 <daniel.riissanen@gmail.com>
2018-03-16 15:02:27 +0200

    Setup JUnit 4

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public String getGreeting() {
+        return "Hello world";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Main().getGreeting());
+    }
+}
