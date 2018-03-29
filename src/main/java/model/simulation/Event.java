37bd9c5b0f64482eaa43f43e45d9ed916826cb34
doc97 <daniel.riissanen@gmail.com>
2018-03-29 21:56:01 +0300

    Add Simulation

---
+++ src/main/java/model/simulation/Event.java
@@ -0,0 +1,44 @@
+package model.simulation;
+
+import model.Postcondition;
+import model.State;
+
+import java.util.HashMap;
+
+/**
+ * Created by Daniel Riissanen on 29.3.2018.
+ */
+public class Event implements Postcondition {
+
+    private HashMap<String, Boolean> keys;
+
+    public Event() {
+        keys = new HashMap<>();
+    }
+
+    public void addKey(String key, boolean value) {
+        keys.put(key, value);
+    }
+
+    public void removeKey(String key) {
+        keys.remove(key);
+    }
+
+    @Override
+    public void activate(State state) {
+        for (String key : keys.keySet())
+            state.apply(key, keys.get(key));
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        if (keys.isEmpty())
+            return "<Empty>";
+
+        for (String key : keys.keySet())
+            builder.append(key).append(": ").append(keys.get(key)).append("\n");
+        builder.deleteCharAt(builder.lastIndexOf("\n"));
+        return builder.toString();
+    }
+}
---
+++ src/main/java/model/simulation/Simulation.java
@@ -0,0 +1,59 @@
+package model.simulation;
+
+import algorithms.PlanningAlgorithm;
+import model.Plan;
+import model.Planner;
+import model.Postcondition;
+import model.Scenario;
+
+/**
+ * Created by Daniel Riissanen on 28.3.2018.
+ */
+public class Simulation {
+
+    private Scenario scenario;
+    private Planner planner;
+    private Plan plan;
+    private boolean isDirty;
+    private boolean isFinished;
+
+    public Simulation(Scenario scenario) {
+        this.scenario = scenario;
+        planner = new Planner();
+        plan = new Plan();
+        isDirty = true;
+    }
+
+    public void plan(PlanningAlgorithm algorithm) {
+        plan = planner.execute(scenario.start, scenario.goal, scenario.actions, algorithm);
+    }
+
+    public void step(Event event) {
+        plan.getNextAction().execute(scenario.start);
+        event.activate(scenario.start);
+        isDirty = true;
+    }
+
+    public boolean isFinished() {
+        if (!isDirty)
+            return isFinished;
+
+        isDirty = false;
+        for (String goalKey : scenario.goal.getKeys()) {
+            if (scenario.start.query(goalKey) != scenario.goal.query(goalKey)) {
+                isFinished = false;
+                return false;
+            }
+        }
+        isFinished = true;
+        return true;
+    }
+
+    public Scenario getScenario() {
+        return scenario;
+    }
+
+    public Plan getPlan() {
+        return plan;
+    }
+}
