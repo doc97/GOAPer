d80a4d1cc8465b4cd3f2e60e73ef53ae16e22051
doc97 <daniel.riissanen@gmail.com>
2018-03-29 21:56:21 +0300

    Add console interface

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,55 +1,7 @@
-import algorithms.NaiveAlgorithm;
-import io.JSONLoader;
-import io.ScenarioLoadFailedException;
-import model.Action;
-import model.Plan;
-import model.Planner;
-import model.Scenario;
+import ui.Console;
 
 public class Main {
     public static void main(String[] args) {
-        JSONLoader loader = new JSONLoader();
-        Scenario scenario;
-        try {
-            scenario = loader.loadScenarioFromFile("res/scenarioExplode.json");
-        } catch (ScenarioLoadFailedException e) {
-            System.err.println("ERROR: Could not load scenario (" + e.getMessage() + ")");
-            return;
-        }
-
-        System.out.println("=====================\n");
-        System.out.println("Starting state:\n" + scenario.start);
-        System.out.println("Goal state:\n" + scenario.goal);
-        System.out.println("Formulating plan...");
-
-        long start = System.nanoTime();
-        boolean finished = true;
-        for (String goalKey : scenario.goal.getKeys()) {
-            if (scenario.start.query(goalKey) != scenario.goal.query(goalKey))
-                finished = false;
-        }
-        if (finished) {
-            System.out.println("SUCCESS: You are already at the goal");
-            return;
-        }
-
-        Planner planner = new Planner();
-        Plan plan = planner.execute(scenario.start, scenario.goal, scenario.actions, new NaiveAlgorithm());
-        long end = System.nanoTime();
-
-        System.out.println("Time elapsed: " + (end - start) / 1000000f + " ms\n");
-
-        if (plan.getActions().length > 0) {
-            System.out.println("SUCCESS: A plan has been made with " + plan.getActions().length + " steps, at the cost of "
-            + plan.getCost() + ".");
-
-            System.out.print("\n[Start] -> ");
-            for (Action a : plan.getActions()) {
-                System.out.print(a + " -> ");
-            }
-            System.out.println("[Goal]");
-        } else {
-            System.out.println("FAILURE: Plan could not be made");
-        }
+        new Console().start();
     }
 }
---
+++ src/main/java/ui/Console.java
@@ -0,0 +1,158 @@
+package ui;
+
+import algorithms.NaiveAlgorithm;
+import algorithms.PlanningAlgorithm;
+import io.JSONLoader;
+import io.ScenarioLoadFailedException;
+import model.Action;
+import model.Scenario;
+import model.simulation.Event;
+import model.simulation.Simulation;
+
+import java.util.Scanner;
+
+/**
+ * Created by Daniel Riissanen on 28.3.2018.
+ */
+public class Console {
+
+    private Simulation simulation;
+    private Event event;
+    private PlanningAlgorithm algorithm;
+    private boolean isRunning;
+
+    public Console() {
+        event = new Event();
+        algorithm = new NaiveAlgorithm();
+    }
+
+    public void start() {
+        isRunning = true;
+        simulation = new Simulation(null);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("===== | CONSOLE | =====");
+        while (isRunning && scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            parseLine(line);
+            System.out.println();
+        }
+    }
+
+    private void stop() {
+        isRunning = false;
+    }
+
+    private void parseLine(String line) {
+        if (line.startsWith("plan")) {
+            if (simulation.getScenario() == null)
+                System.out.println("No scenario loaded");
+            else
+                simulation.plan(algorithm);
+        } else if (line.startsWith("step")) {
+            if (simulation.getScenario() == null)
+                System.out.println("No scenario loaded");
+            else
+                simulation.step(event);
+        } else if (line.startsWith("show")) {
+            String[] arr = line.split(" ", 2);
+            if (arr.length < 2) {
+                System.out.println("show: Invalid argument, try 'plan', 'scenario' or 'event'");
+                return;
+            }
+
+            if (simulation.getScenario() == null) {
+                System.out.println("No scenario loaded");
+                return;
+            }
+
+            String arg = arr[1];
+            switch (arg) {
+                case "plan":
+                    System.out.println(simulation.getPlan());
+                    break;
+                case "cost":
+                    System.out.println(simulation.getPlan().getCost());
+                case "situation":
+                    System.out.println(simulation.getScenario().start);
+                    break;
+                case "goal":
+                    System.out.println(simulation.getScenario().goal);
+                    break;
+                case "actions":
+                    for (Action a : simulation.getScenario().actions)
+                        System.out.println(a);
+                    break;
+                case "event":
+                    String eventStr = event.toString();
+                    if (!eventStr.isEmpty())
+                        System.out.println(eventStr);
+                    break;
+                case "status":
+                    System.out.println(simulation.isFinished() ? "Finished" : "Not finished");
+                    break;
+                default:
+                    System.out.println("show: Invalid argument, try 'plan', 'scenario' or 'event'");
+                    break;
+            }
+        } else if (line.startsWith("set")) {
+            String[] arr = line.split(" ", 3);
+            if (arr.length < 3 || arr[1].isEmpty()) {
+                System.out.println("set: Invalid arguments");
+                return;
+            }
+
+            String key = arr[1];
+            boolean value = Boolean.parseBoolean(arr[2]);
+            event.addKey(key, value);
+            System.out.println(key + " set to " + value);
+        } else if (line.startsWith("unset")) {
+            String[] arr = line.split(" ", 2);
+            if (arr.length < 2 || arr[1].isEmpty()) {
+                System.out.println("unset: Invalid argument");
+                return;
+            }
+
+            event.removeKey(arr[1]);
+        } else if (line.startsWith("exit")) {
+            stop();
+        } else if (line.startsWith("load")) {
+            String[] arr = line.split(" ", 2);
+            String filename = arr.length > 1 ? arr[1] : "";
+            loadScenario(filename);
+        } else if (line.startsWith("help")) {
+            String help =
+                    "load <file>\n" +
+                    "\tLoad a scenario from a file\n" +
+                    "plan\n" +
+                    "\tFormulate a plan from the current scenario\n" +
+                    "step\n" +
+                    "\tStep through the simulation, will activate and consume the event\n" +
+                    "show <plan/cost/situation/goal/actions/event/status>\n" +
+                    "\tShow information\n" +
+                    "set <key> <value>\n" +
+                    "\tSet a boolean value with a key for the next event\n" +
+                    "unset <key>\n" +
+                    "\tUnset a value with a key from the next event\n" +
+                    "help\n" +
+                    "\tShows this help\n" +
+                    "exit\n" +
+                    "\tExits the program";
+            System.out.println(help);
+        } else if (!line.isEmpty()) {
+            System.out.println(line + ": Command not found");
+        }
+    }
+
+    private void loadScenario(String filename) {
+        JSONLoader loader = new JSONLoader();
+        Scenario scenario;
+        try {
+            scenario = loader.loadScenarioFromFile(filename);
+        } catch (ScenarioLoadFailedException e) {
+            System.out.println("ERROR: Could not load scenario (" + e.getMessage() + ")");
+            return;
+        }
+
+        simulation = new Simulation(scenario);
+    }
+}
